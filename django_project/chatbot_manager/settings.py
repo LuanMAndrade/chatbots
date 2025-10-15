4b972487d4fd64141e7d4f97a94ac95d152c68ae
LuanMAndrade <luanmunizandrade@gmail.com>
2025-10-15 11:36:41 +0000

    consegui subir o django

--- django_project/chatbot_manager/settings.py
+++ django_project/chatbot_manager/settings.py
@@ -20,6 +20,17 @@ DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+# CSRF Settings - ADICIONADO
+CSRF_TRUSTED_ORIGINS = [
+    'https://chatbotsimas.shop',
+    'http://chatbotsimas.shop',
+    'https://*.chatbotsimas.shop',
+]
+
+# Se estiver usando proxy reverso (nginx, etc)
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
@@ -114,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
